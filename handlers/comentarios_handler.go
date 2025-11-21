a8a18286d850ac1d18e5806e70b0bd9a9a0c121e
Neveslv <enzoematheus092009@gmail.com>
2025-11-21 11:36:09 -0300

    Subindo a struct necessaria para a criação do handler de comentario(CRUD) e a estrutura dele.

--- handlers/comentarios_handler.go
+++ handlers/comentarios_handler.go
@@ -1,1 +1,23 @@
 package handlers
+
+import "github.com/gin-gonic/gin"
+
+func CriarComentario(c *gin.Context) {
+
+}
+
+func ListarComentario(c *gin.Context) {
+
+}
+
+func BuscarComentarioPorId(c *gin.Context) {
+
+}
+
+func AtualizarComentario(c *gin.Context) {
+
+}
+
+func DeletarComentario(c *gin.Context) {
+
+}
--- model/comentarios.go
+++ model/comentarios.go
@@ -8,4 +8,11 @@ type Comentarios struct {
 	UserID    int       `json:"user_id" binding:"required"`
 	Content   string    `json:"content" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
+
+	UserNome  string `json:"user_nome,omitempty"`
+	UserEmail string `json:"user_email,omitempty"`
+}
+
+type ComentarioImput struct {
+	Content string `json:"content" binding:"required,min=1 max=1000"`
 }
