b91aaad28dc436d0cb4ec572cdfbcde55b9a1f29
Neveslv <enzoematheus092009@gmail.com>
2025-12-02 10:14:31 -0300

    Adaptando o handler(CRUD) de post para enviar respostas ao formulário e ao banco de dados, e corrigindo o routes.go para o acesso aos HTML de posts darem certo.

--- handlers/post_handler.go
+++ handlers/post_handler.go
@@ -12,26 +12,71 @@ import (
 func CriarPost(c *gin.Context) {
 	var input model.PostInput
 
-	if err := c.ShouldBindJSON(&input); err != nil {
+	contentType := c.GetHeader("Content-Type")
+	if contentType == "application/json" {
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":    "JSON inválido",
+				"detalhes": err.Error(),
+			})
+			return
+		}
+	} else {
+		input.Titulo = c.PostForm("titulo")
+		input.Content = c.PostForm("content")
+	}
+	if input.Titulo == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":    err.Error(),
-			"detalhes": err.Error(),
+			"error": "O campo titulo é obrigatório",
 		})
 		return
 	}
 
-	userIDStr := c.Query("user_id")
-	if userIDStr == "" {
+	if input.Content == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "user_id é obrigatório",
+			"error": "O campo content é obrigatório",
 		})
 		return
 	}
-	userID, _ := strconv.Atoi(userIDStr)
+
+	var userID int
+	var err error
+	if contentType == "application/json" {
+
+		userIDStr := c.Query("user_id")
+		if userIDStr == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "user_id é obrigatório",
+			})
+			return
+		}
+		userID, err = strconv.Atoi(userIDStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "user_id inválido",
+			})
+			return
+		}
+	} else {
+		userIDStr := c.PostForm("user_id")
+		if userIDStr == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "user_id inválido",
+			})
+			return
+		}
+		userID, err = strconv.Atoi(userIDStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "user_id inválido",
+			})
+			return
+		}
+	}
 
 	var postID int
 	query := "INSERT INTO posts (user_id, titulo, content) VALUES ($1, $2, $3) RETURNING id"
-	err := database.DB.QueryRow(query, userID, input.Titulo, input.Content).Scan(&postID)
+	err = database.DB.QueryRow(query, userID, input.Titulo, input.Content).Scan(&postID)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -40,15 +85,18 @@ func CriarPost(c *gin.Context) {
 		})
 		return
 	}
-
-	c.JSON(http.StatusCreated, gin.H{
-		"mensagem": "Post foi criado com sucesso.",
-		"id":       postID,
-	})
+	if contentType == "application/json" {
+		c.JSON(http.StatusCreated, gin.H{
+			"mensagem": "Post foi criado com sucesso.",
+			"id":       postID,
+		})
+	} else {
+		c.Redirect(http.StatusFound, "/posts")
+	}
 }
 
 func ListarPosts(c *gin.Context) {
-	rows, err := database.DB.Query("SELECT p.id, p.user_id, p.content, p.created_at, u.nome, u.email FROM posts p INNER JOIN usuario u ON p.user_id = u.id ORDER BY p.created_at DESC ")
+	rows, err := database.DB.Query("SELECT p.id, p.user_id, p.titulo,  p.content, p.created_at, u.nome, u.email FROM posts p INNER JOIN usuario u ON p.user_id = u.id ORDER BY p.created_at DESC ")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":    "Erro ao buscar posts",
@@ -98,7 +146,7 @@ func BuscarPostPorId(c *gin.Context) {
 func ListarPostPorUsuario(c *gin.Context) {
 	userID := c.Param("user_id")
 
-	rows, err := database.DB.Query("SELECT p.id, p.user_id, p.content, p.created_at, u.nome, u.email FROM posts p INNER JOIN usuario u ON p.user_id = u.id WHERE p.user_id=$1 ORDER BY p.created_at DESC ", userID)
+	rows, err := database.DB.Query("SELECT p.id, p.user_id, p.titulo, p.content, p.created_at, u.nome, u.email FROM posts p INNER JOIN usuario u ON p.user_id = u.id WHERE p.user_id=$1 ORDER BY p.created_at DESC ", userID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":    "Erro ao buscar posts",
@@ -130,13 +178,28 @@ func ListarPostPorUsuario(c *gin.Context) {
 
 func AtualizarPost(c *gin.Context) {
 	id := c.Param("id")
-
 	var input model.PostInput
-	if err := c.ShouldBindJSON(&input); err != nil {
+
+	contentType := c.GetHeader("Content-Type")
+
+	if contentType == c.GetHeader("application/json") {
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":    "Dados inválidos",
+				"detalhes": err.Error(),
+			})
+			return
+		}
+	} else {
+		input.Titulo = c.PostForm("titulo")
+		input.Content = c.PostForm("content")
+	}
+
+	if input.Titulo == "" || input.Content == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":    "Dados inválidos",
-			"detalhes": err.Error(),
+			"error": "Os campos 'titulo' e 'content' são obrigatórios",
 		})
+		return
 	}
 
 	result, err := database.DB.Exec("UPDATE posts SET titulo = $1, content = $2 WHERE id = $3", input.Titulo, input.Content, id)
@@ -155,9 +218,13 @@ func AtualizarPost(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"mensagem": "Post atualizado com sucesso",
-	})
+	if contentType == "application/json" {
+		c.JSON(http.StatusOK, gin.H{
+			"mensagem": "Post atualizado com sucesso",
+		})
+	} else {
+		c.Redirect(http.StatusFound, "/posts")
+	}
 }
 
 func DeletarPost(c *gin.Context) {
@@ -165,7 +232,8 @@ func DeletarPost(c *gin.Context) {
 	result, err := database.DB.Exec("DELETE post WHERE id = $1`", id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Erro ao deletar o post",
+			"error":    "Erro ao deletar o post",
+			"detalhes": err.Error(),
 		})
 		return
 	}
--- routes/routes.go
+++ routes/routes.go
@@ -8,15 +8,7 @@ import (
 
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"mensagem": "API CrudGO está rodando!",
-			"versao":   "1.0",
-			"rotas": gin.H{
-				"usuarios":    "/api/usuarios",
-				"posts":       "/api/posts",
-				"comentarios": "/api/comentarios",
-			},
-		})
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	r.GET("/usuarios", func(c *gin.Context) {
@@ -29,10 +21,10 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/posts", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "posts.html", nil)
 	})
-	r.GET("/post/novo", func(c *gin.Context) {
+	r.GET("/posts/novo", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "post_form.html", nil)
 	})
-	r.GET("/post/:id/detalhes", func(c *gin.Context) {
+	r.GET("/posts/:id/detalhes", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "post_detalhes.html", nil)
 	})
 
